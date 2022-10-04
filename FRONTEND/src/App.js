bee707b97f9969b487cebb5320c14f0a6260c7e8
charlyguz <gusmanc622@gmail.com>
2022-10-03 20:37:08 -0500

    conexion a metamask

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, {useState} from "react";
+import {ethers} from "ethers";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [currentAccount, setCurrentAccount] = useState("");
+  const initConnection = async () => {
+    if(typeof window.etherium !== "undefined"){
+      console.log("MetaMask is installed");
+      const accounts = await window.ethereum.request({method: "eth_requestAccounts"});
+      console.log(accounts[0]);
+      setCurrentAccount(accounts[0]);
+    }else{
+      return(
+        console.log("MetaMask is not installed")
+      )
+    }
+  }
+  return(
+    <div>
+      <button onClick={initConnection}>Connect</button>
+      <p>{currentAccount}</p>
     </div>
-  );
+  )
 }
 
 export default App;
