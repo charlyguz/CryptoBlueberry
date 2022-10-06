9d4ef607ec70e8a0794b32f8e3090f65584906c0
Aaron Rincon <rlaaron0305@gmail.com>
2022-10-05 22:40:04 -0500

    update functions

--- FRONTEND/src/pages/dashboard/dashboardComponets/suscripciones/Suscripciones.js
+++ FRONTEND/src/pages/dashboard/dashboardComponets/suscripciones/Suscripciones.js
@@ -1,7 +1,26 @@
 import React from "react";
 import "./suscripciones.css";
+import {getProviderPlans,getAllGroups, createGroup, joinGroup} from "../../utils/berry-contract";
 
-const Suscripciones = () => {
+
+const Suscripciones = ({signer}) => {
+  const plansProvider = async function getProviderPlansD(signer, providerId) {
+    const plans = await getProviderPlans(signer, providerId);
+    return plans;
+  }
+  
+  const allGroups = async function getAllGroupsD(signer) {
+    const groups = await getAllGroups(signer);
+    return groups;
+  }
+
+  async function createGroupD(signer, providerId, planId, name) {
+    await createGroup(signer, providerId, planId, name);
+  }
+  async function joinGroupD(signer, groupId) {
+    await joinGroup(signer, groupId);
+  }
+
   return (
     <section className="section__suscripciones-container">
         <h3 className="section__suscripciones-item-title">Suscripciones</h3>
