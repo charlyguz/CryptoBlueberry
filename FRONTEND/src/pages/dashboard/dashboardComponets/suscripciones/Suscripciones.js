ba10104b979a1e375d22f3c782e049759dc1bd07
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-10-06 10:31:45 -0500

    Funcion para todos los grupos de usuario

--- FRONTEND/src/pages/dashboard/dashboardComponets/suscripciones/Suscripciones.js
+++ FRONTEND/src/pages/dashboard/dashboardComponets/suscripciones/Suscripciones.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./suscripciones.css";
-import { Cards } from "./Cards";
+import { Cards } from './Cards'
+import { ethers } from "ethers";
 // import {getProviderPlans,getAllGroups, createGroup, joinGroup} from "../../utils/berry-contract";
+import abi from "../../../../contract/abi.json";
 
-const Suscripciones = ({signer}) => {
+const addres = '0xf25137694E130Fb87735a87C49691054a34cD930'
+
+const Suscripciones = ({ signer }) => {
+  const [misGrupos, setMisGrupos] = useState([])
+  
+  async function getMyGroups() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        addres,
+        abi.abi,
+        signer
+      );
+      try {
+         setMisGrupos(await contract.getUserGroups(contract, contract.signer))
+        console.log("provider creado");
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+  }
+  
   // const plansProvider = async function getProviderPlansD(signer, providerId) {
   //   const plans = await getProviderPlans(signer, providerId);
   //   return plans;
   // }
-  
+
   // const allGroups = async function getAllGroupsD(signer) {
   //   const groups = await getAllGroups(signer);
   //   return groups;
@@ -23,7 +48,7 @@ const Suscripciones = ({signer}) => {
 
   return (
     <>
-    <section className="section__suscripciones-container">
+      <section className="section__suscripciones-container">
         <h3 className="section__suscripciones-item-title">Suscripciones</h3>
         <div className="section__suscripciones-item">
           <Cards image={require("./../../../../assets/img/Coursera.svg").default} title="Coursera"/>
@@ -32,7 +57,7 @@ const Suscripciones = ({signer}) => {
           <Cards image={require("./../../../../assets/img/amazon.svg").default} title="Amazon"/>
           <Cards image={require("./../../../../assets/img/hbo.svg").default} title="HBO"/>
         </div>
-    </section>
+      </section>
     </>
   );
 };
