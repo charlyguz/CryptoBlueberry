d905d4a4ba2e321f54b513e3882ae9f7e9928b2a
Aaron Rincon <rlaaron0305@gmail.com>
2022-10-06 01:31:10 -0500

    maybe add

--- FRONTEND/src/pages/dashboard/dashboardComponets/grupos/Grupos.js
+++ FRONTEND/src/pages/dashboard/dashboardComponets/grupos/Grupos.js
@@ -1,18 +1,38 @@
 import React from "react";
 import "./grupos.css";
-import {getUserGroups} from "../../../utils/berry-contract";
+// import {getUserGroups} from "../../../utils/berry-contract";
 
-const Grupos = ({signer}) => {
+const addres = ''
+// const Grupos = ({signer}) => {
+const Grupos = () => {
 
-  const groups = async function getUserGroupsD(signer) {
-    const groups = await getUserGroups(signer);
-    return groups;
-  }
+  // const groups = async function getUserGroupsD(signer) {
+  //   const groups = await getUserGroups(signer);
+  //   return groups;
+  // }
+
+  // const groups = async function getUserGroupsD() {
+  //   if(window.rsk){
+  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //     const signer = provider.getSigner();
+  //     const contract = new ethers.Contract(
+  //       addres,
+  //       ABI.abi,
+  //       signer
+  //     );
+  //     try {
+  //       const groups = await contract.getUserGroups();
+  //       return groups;
+  //     }
+  //   }
+  // }
+
 
   return (
     <section className="section__grupos-container">
       <h3 className="section__grupos-item-title">Grupos</h3>
-      <div className="section__grupos-item">{groups(signer)}</div>
+      {/* <div className="section__grupos-item">{groups(signer)}</div> */}
+      <div className="section__grupos-item"></div>
     </section>
   );
 };
