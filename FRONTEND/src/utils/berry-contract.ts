ac2cbca055b5951a14d2e6c02361ae2890dec354
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-10-05 20:36:36 -0500

    Finish bridge functions

--- FRONTEND/src/utils/berry-contract.ts
+++ FRONTEND/src/utils/berry-contract.ts
@@ -7,26 +7,50 @@ export const berry = Berry__factory.connect(process.env.REACT_APP_BERRY_CONTRACT
 
 export type SignerOrProvider = Signer | Provider;
 
+export type Group = Awaited<ReturnType<Berry['groups']>>
+export type ServiceProvider = Awaited<ReturnType<Berry['providers']>>
+export type SubscriptionPlan = Awaited<ReturnType<Berry['plansPerProvider']>>
+export type User = Awaited<ReturnType<Berry['users']>>
+
+
+// Owner actions
+export const createProvider = async (ownerAccount: Signer, ...createProviderArgs: Parameters<Berry['createProvider']>) => {
+  return berry.connect(ownerAccount).createProvider(...createProviderArgs)
+}
+
+// Service Provider actions
+export const createPlan = async (providerAccount: Signer, ...planArgs: Parameters<Berry['addPlan']>) => {
+  return berry.connect(providerAccount).addPlan(...planArgs)
+}
+
+// User actions
+export const register = async (userAccount: Signer, ...registerArgs: Parameters<Berry['register']>) => {
+  return berry.connect(userAccount).register(...registerArgs)
+}
+
+export const createGroup = async (userAccount: Signer, ...createGroupArgs: Parameters<Berry['createGroup']>) => {
+  return berry.connect(userAccount).createGroup(...createGroupArgs)
+}
+
+export const joinGroup = async (userAccount: Signer, groupID: number) => {
+  return berry.connect(await userAccount.getAddress()).joinGroup(groupID)
+}
+
+// Public actions
 export const getAllProviders = async () => {
   const totalProviders = (await berry.numProviders()).toNumber()
 
   return Promise.all(Array.from({ length: totalProviders }, async (_, providerID) => await berry.providers(providerID)))
 }
 
-export const createProvider = async (ownerAccount: Signer, ...createProviderArgs: Parameters<Berry['createProvider']>) => {
-  
-}
-
-// Description
-// Image
-export const register = async (userAccount: Signer, ...registerArgs: Parameters<Berry['register']>) => {
-  await berry.connect(userAccount).register(...registerArgs)
-}
-
 export const getAllGroups = async () => {
   const totalGroups = (await berry.numGroups()).toNumber()
 
   return Promise.all(Array.from({ length: totalGroups }, async (_, groupID) => await berry.groups(groupID)))
+}
+
+export const getPlan = async (providerID: number, planID: number) => {
+  return berry.plansPerProvider(providerID, planID)
 }
 
 export const getProviderPlans = async (providerID: BigNumberish) => {
@@ -44,18 +68,25 @@ export const getUserGroups = async (userAccount: Signer) => {
   return Promise.all(Array.from({ length: userGroupCount }, async (_, groupID) => await berry.groupsPerUser(userAddress, groupID)))
 }
 
-export const createGroup = async (userAccount: Signer, ...createGroupArgs: Parameters<Berry['createGroup']>) => {
-  return berry.connect(userAccount).createGroup(...createGroupArgs)
+// Post-processing methods
+// Needs all groups
+export const getGroupsPerPlan = (allGroups: Awaited<ReturnType<typeof getAllGroups>>) => {
+  const groupsPerPlan: Record<number, Group[]> = {}
+
+  allGroups.forEach((group) => {
+    let groupsInPlan = groupsPerPlan[group.planID.toNumber()]
+
+    if (groupsInPlan) {
+      groupsInPlan.push(group)
+    } else {
+      groupsInPlan = [group]
+    }
+  })
+
+  return groupsPerPlan;
 }
 
-export const getPlan = async (providerID: number, planID: number) => {
-  return berry.plansPerProvider(providerID, planID)
-}
-
-export const joinGroup = async (userAccount: Signer, groupID: number) => {
-  return berry.connect(await userAccount.getAddress()).joinGroup(groupID)
-}
-
-export const createPlan = async (providerAccount: Signer, ...planArgs: Parameters<Berry['addPlan']>) => {
-  return berry.connect(providerAccount).addPlan(...planArgs)
+// Needs all providers
+export const getAllPlans =  async (allProviders: Awaited<ReturnType<typeof getAllProviders>>) => {
+  return (await Promise.all(allProviders.map(async (serviceProvider) => await getProviderPlans(serviceProvider.providerID)))).flat()
 }
