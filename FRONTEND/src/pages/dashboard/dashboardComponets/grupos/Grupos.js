975ffe83010626403957d039d281ef81c3ab56c8
Aaron Rincon <rlaaron0305@gmail.com>
2022-10-05 22:39:59 -0500

    update functions

--- FRONTEND/src/pages/dashboard/dashboardComponets/grupos/Grupos.js
+++ FRONTEND/src/pages/dashboard/dashboardComponets/grupos/Grupos.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./grupos.css";
+import {getUserGroups} from "../../../utils/berry-contract";
 
-const Grupos = () => {
+const Grupos = ({signer}) => {
+
+  const groups = async function getUserGroupsD(signer) {
+    const groups = await getUserGroups(signer);
+    return groups;
+  }
+
   return (
     <section className="section__grupos-container">
       <h3 className="section__grupos-item-title">Grupos</h3>
-      <div className="section__grupos-item"></div>
+      <div className="section__grupos-item">{groups(signer)}</div>
     </section>
   );
 };
