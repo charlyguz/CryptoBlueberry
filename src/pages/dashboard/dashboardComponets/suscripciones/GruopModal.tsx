f400f309e060bdcfa5663d4a438fb887453afff7
devleon <devleon00@gmail.com>
2022-11-03 00:36:58 -0600

    Agrego modal de crear grupo

---
+++ src/pages/dashboard/dashboardComponets/suscripciones/GruopModal.tsx
@@ -0,0 +1,106 @@
+/* This example requires Tailwind CSS v2.0+ */
+import React, { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { ExclamationIcon, XIcon } from "@heroicons/react/outline";
+import { BigNumber, BigNumberish } from "ethers";
+
+export default function GruopModal({
+  planId,
+  providerId,
+  open,
+  setOpen,
+}: {
+  planId: BigNumberish;
+  providerId: BigNumberish;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed z-10 inset-0 overflow-y-auto"
+        onClose={setOpen}
+      >
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full sm:p-6">
+              <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+                <button
+                  type="button"
+                  className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  onClick={() => setOpen(false)}
+                >
+                  <span className="sr-only">Close</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className=" w-100">
+                <div className="w-100">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Nombre del grupo
+                  </label>
+                  <div className="mt-1 w-100">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md p-1"
+                      placeholder="Nombre del grupo"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  // onClick={() => setOpen(false)}
+                >
+                  Crear Grupo
+                </button>
+                {/* <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                  onClick={() => setOpen(false)}
+                >
+                  Cancel
+                </button> */}
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
--- src/pages/dashboard/dashboardComponets/suscripciones/SubscripcionCard.tsx
+++ src/pages/dashboard/dashboardComponets/suscripciones/SubscripcionCard.tsx
@@ -1,52 +1,84 @@
-import { ethers } from 'ethers'
-import React, { useState, useEffect } from 'react'
-import { ServiceProvider, SubscriptionPlan, PlanWithProvider } from '../../../../utils/berry-contract'
+import { ethers } from "ethers";
+import React, { useState, useEffect } from "react";
+import GruopModal from "./GruopModal";
 
+import {
+  ServiceProvider,
+  SubscriptionPlan,
+  PlanWithProvider,
+} from "../../../../utils/berry-contract";
 
+export default function SubscripcionCard({
+  planWithProvider,
+  index,
+}: {
+  planWithProvider: PlanWithProvider;
+  index: number;
+}) {
+  const [open, setOpen] = useState(false);
 
-export default function SubscripcionCard({ planWithProvider, index }: { planWithProvider: PlanWithProvider, index: number }) {
   return (
     <React.Fragment>
-      <li key={index} className="py-10 px-6 bg-slate-500 text-center rounded-lg">
+      <GruopModal planId={1} providerId={1} open={open} setOpen={setOpen}></GruopModal>
+      <li
+        key={index}
+        className="py-10 px-6 bg-slate-500 text-center rounded-lg"
+      >
         <div className="space-y-6 ">
-          <img className="mx-auto h-40 w-40 rounded-full" src={planWithProvider.provider.imageURL} alt="provider" />
+          <img
+            className="mx-auto h-40 w-40 rounded-full"
+            src={planWithProvider.provider.imageURL}
+            alt="provider"
+          />
           <div className="space-y-2">
             <div className="font-medium leading-6 space-y-1">
               <h3 className="text-white text-lg">{planWithProvider.name}</h3>
-              <p className="text-indigo-200 text-md">{planWithProvider.description}</p>
+              <p className="text-indigo-200 text-md">
+                {planWithProvider.description}
+              </p>
             </div>
             <br />
-            <dl className="mt-8 divide-y divide-gray-100 text-sm lg:mt-0 lg:col-span-5">
+            <dl className="mt-8 divide-y divide-gray-100 text-sm lg:mt-0 lg:col-span-5 space-b-5">
               <div className="pb-4 flex items-center justify-between">
                 <dt className="text-gray-200">Precio de subscripción</dt>
-                <dd className="font-medium text-gray-300">${ethers.utils.formatEther(planWithProvider.price)} ETH</dd>
+                <dd className="font-medium text-gray-300">
+                  ${ethers.utils.formatEther(planWithProvider.price)} ETH
+                </dd>
               </div>
               <div className="py-4 flex items-center justify-between">
                 <dt className="text-gray-200">Precio por miembro</dt>
-                <dd className="font-medium text-gray-300">${ethers.utils.formatEther(planWithProvider.pricePerMember)} ETH</dd>
+                <dd className="font-medium text-gray-300">
+                  ${ethers.utils.formatEther(planWithProvider.pricePerMember)}{" "}
+                  ETH
+                </dd>
               </div>
               <div className="py-4 flex items-center justify-between">
                 <dt className="text-gray-200">Máximo de miembros</dt>
-                <dd className="font-medium text-gray-300">{planWithProvider.maxMembers.toString()}</dd>
+                <dd className="font-medium text-gray-300">
+                  {planWithProvider.maxMembers.toString()}
+                </dd>
               </div>
               <div className="pt-4 flex items-center justify-between">
                 <dt className="font-medium text-gray-200">Pagar cada</dt>
-                <dd className="font-medium text-gray-300">{planWithProvider.recurrence.toString()} días</dd>
+                <dd className="font-medium text-gray-300">
+                  {planWithProvider.recurrence.toString()} días
+                </dd>
               </div>
             </dl>
-            {/* <ul className="flex justify-around space-x-5">
+
+            <ul className="flex justify-around space-x-5 space-y-5">
               <li>
                 <span
-                  className="px-4 py-2 rounded-full bg-green-400 text-gray-100 font-bold">
+                  className="px-4 py-2 rounded-full bg-green-400 text-gray-100 font-bold"
+                  onClick={() => setOpen(true)}
+                >
                   ${ethers.utils.formatEther(planWithProvider.price)} ETH
                 </span>
               </li>
-              <li>
-              </li>
-            </ul> */}
+            </ul>
           </div>
         </div>
       </li>
     </React.Fragment>
-  )
+  );
 }
